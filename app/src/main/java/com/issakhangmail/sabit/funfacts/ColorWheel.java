d2cec106afd90b2e4d1b42f4c16f8ec9d18905a7
sabit.issakhan@gmail.com <titan2092s>
2017-05-08 20:04:00 +0600

    Add white color

--- app/src/main/java/com/issakhangmail/sabit/funfacts/ColorWheel.java
+++ app/src/main/java/com/issakhangmail/sabit/funfacts/ColorWheel.java
@@ -23,7 +23,9 @@ public class ColorWheel {
         "#e0ab18", // mustard
         "#637a91", // dark gray
         "#f092b0", // pink
-        "#b7c0c7"  // light gray
+        "#b7c0c7",  // light gray
+             "#ffffff" // White Color
+
 };
     public int getColor() {
 
